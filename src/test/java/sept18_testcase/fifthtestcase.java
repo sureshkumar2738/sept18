40a48d0321c1b12aee28a8d1a074cc92a75f1749
suresh kumar <pennadasuresh@gmail.com>
2021-03-22 12:24:06 +0530

    fourth commit....

---
+++ src/test/java/sept18_testcase/fifthtestcase.java
@@ -0,0 +1,27 @@
+package sept18_testcase;
+
+import org.testng.annotations.Test;
+
+public class fifthtestcase {
+
+	@Test
+	public void Register() {
+		
+		System.out.println("Register is done");
+		
+	}
+	
+	@Test
+	public void Login() {
+		
+		System.out.println("Login is done");
+		
+	}
+	
+	@Test
+	public void Close() {
+		
+		System.out.println("Close is done");
+		
+	}
+}
